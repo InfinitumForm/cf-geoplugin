31d5d83b9f021afdef29a9e2f78a7d1c34af87cd
DAnn2012 <dannunziata@hotmail.it>
2026-01-18 12:57:23 +0100

    Update Public.php

--- inc/classes/Public.php
+++ inc/classes/Public.php
@@ -200,7 +200,7 @@ if (!class_exists('CFGP_Public', false)) : class CFGP_Public extends CFGP_Global
 				})
 			}).catch((error) => {
 				console.error('<?php
-                    esc_attr__e('The geo controller could not update the CSS.', 'cf-geoplugin');
+                    echo esc_js( __( 'The geo controller could not update the CSS.', 'cf-geoplugin' ) );
         ?>');
 				
 				setTimeout(function(){
